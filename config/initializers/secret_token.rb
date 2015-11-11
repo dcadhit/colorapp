8a297584f3ea4629fd9fa3aa11b37139ca2a519f
Danielle Cadhit <dcadhit@outlook.com>
2015-11-11 20:47:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Colorapp::Application.config.secret_key_base = 'bd5341faf84affd8c5d4eff481cc29eca81d8f361d82d2316cd9a9ddb375f7f9f6908c27f2c34fc7a4741481910861ca85d651143f4c4479efa2ed8469f1a000'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Colorapp::Application.config.session_store :cookie_store, key: '_colorapp_session'
